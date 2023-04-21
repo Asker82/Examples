f9493dfd3c60bd60def02e189e8aa74328de22e0
Asker82 <kkerra@mail.ru>
2023-04-21 11:45:02 +0300

    add Rendome

--- Exaple003_Sum/Program.cs
+++ Exaple003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int namberA = 1;
-int namberB = 5;
-int result = namberA + namberB;
-Console.WriteLine(namberA + namberB);
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(numberA + numberB);
